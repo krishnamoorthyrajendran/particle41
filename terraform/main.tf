054c552eb48aa1675ae7bad7486109ca3cb9ae0a
krishnamoorthy <76277285+krishnamoorthyrajendran@users.noreply.github.com>
2025-04-16 21:43:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,15 +13,33 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "simple-time-service-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-   
-    dynamodb_table = "simple-time-service-terraform-locks"  # Keep this for now
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "simple-time-service-terraform-state"
+  
+  lifecycle {
+    prevent_destroy = true
   }
 }
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "simple-time-service-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 # VPC Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
