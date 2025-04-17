06555a8d870016d8ddd60d3194508c08086e621c
krishnamoorthy <krishnamoorthy@pinnacleseven.com>
2025-04-17 07:11:05 +0530

    task

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,43 +13,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "test-bucket-pratcle-new"  # Replace with a unique bucket name
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform Locks Table"
-    Environment = "Dev"
-  }
-}
 
 terraform {
   backend "s3" {
